3471c5bd6f58e70424d5ee8fb3679e7126550a99
Ivan Karapuzoff <ivan@karapuzoff.net>
2014-12-26 15:30:02 +0300

    fix css
    "containet" problem

--- js/borderMenu.js
+++ js/borderMenu.js
@@ -11,8 +11,6 @@
 
 		var menu = document.getElementById( 'bt-menu' ),
 			trigger = menu.querySelector( 'a.bt-menu-trigger' ),
-			// triggerPlay only for demo 6
-			triggerPlay = document.querySelector( 'a.bt-menu-trigger-out' ),
 			// event type (if mobile use touch events)
 			eventtype = mobilecheck() ? 'touchstart' : 'click',
 			resetMenu = function() {
